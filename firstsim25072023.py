77ce6b2c0b6291a5cea7b9ef271e95a04cb5ce4e
Your Name <you@example.com>
2023-07-25 20:36:41 +0300

    lalalala

---
+++ firstsim25072023.py
@@ -0,0 +1,20 @@
+my_cat = 'my cat'
+
+my_greeting = "i'm David"
+my_greeting2= "i\'m \n David and \t"
+print(my_greeting2)
+
+chunk = """
+laalllalal
+allalalalalal
+allalalalalaalal
+alalalalalallala
+"""
+
+myltiplicated = my_cat * 15
+print (myltiplicated)
+print('*' * 150)
+
+summa = my_greeting + my_cat
+print(summa)
+print(chunk)
